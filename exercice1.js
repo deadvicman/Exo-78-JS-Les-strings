5aea00963cfc9f453c24cef7bfd45388d6181c40
victorien <deadvicmangame@gmail.com>
2022-12-01 13:34:40 +0100

    exo 78

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,28 @@
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
-
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let position = monTexte.indexOf('i');
+console.log(position);
+document.getElementById('position');
+monTexte.indexOf('i');
+console.log(position);
+
+let nouvelle = monTexte.toUpperCase();
+document.getElementById("chaineMaj");
+monTexte.toUpperCase();
+console.log(nouvelle);
+monTexte.toUpperCase();
+
+for(let i=0; i<monTexte.indexOf; i++){
+    alert("lettre a trouvée"[i]);
+}
+
+
+
 
 
 
